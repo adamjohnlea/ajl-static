dedaa8b33784e408237f379e25ad8c798077b7ee
adamjohnlea <imnogeekdptcom@me.com>
2025-06-28 22:09:33 -0600

    add dynamic current year

--- include.js
+++ include.js
@@ -2,6 +2,11 @@ async function loadHTML(id, url) {
   const res = await fetch(url);
   const text = await res.text();
   document.getElementById(id).innerHTML = text;
+
+  // If this is the footer, set the current year
+  if (id === "footer") {
+    document.getElementById("current-year").textContent = new Date().getFullYear();
+  }
 }
 
 loadHTML("header", "partials/header.html");
